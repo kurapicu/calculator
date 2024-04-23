0b004d22da7d010157580e8d98eafdafce02d8e4
Nathan <youremail@>
2024-04-23 11:14:33 +0100

    fish

--- HelloController.java
+++ HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Applicationk!");
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
